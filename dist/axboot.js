7228abbb3d6de82573c28f51b595c4a1e9198b48
ThomasJ <tom@axisj.com>
2016-02-25 16:01:48 +0900

    dist

---
+++ dist/axboot.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function () {
+
+    var axboot = {
+        getJSON: function getJSON() {
+            return function (json) {
+                return json ? json : false;
+            };
+        }
+    };
+
+    this.axboot = axboot;
+}).call(window);
+(function () {
+
+    var builder = {};
+
+    if (!this.axboot) {
+        this.axboot = {};
+    }
+
+    this.axboot.builder = builder;
+}).call(window);
